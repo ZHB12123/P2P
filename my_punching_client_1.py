f92cb4a12d7ed0b842f754e0a47944b80b85b9a1
ZHB12123 <434323746@qq.com>
2024-03-25 00:33:48 +0800

    Add files via upload

---
+++ my_punching_client_1.py
@@ -0,0 +1,72 @@
+import socket
+import time
+import json
+import threading
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# sock.bind(('0.0.0.0', 18880))
+
+
+recv_msg=""
+
+def recv_func(_sock):
+    global recv_msg
+    while True:
+        data, server = _sock.recvfrom(4096)
+        print(data.decode("utf-8"), server)
+        recv_msg=data.decode("utf-8")
+        
+
+
+identifier="client2"
+
+data = {"COMMAND": "register", "key": "zhb1", "identifier": identifier}
+msg = json.dumps(data).encode()
+sent = sock.sendto(msg, ('175.178.109.217', 8478))
+time.sleep(1)
+
+
+t = threading.Thread(target=recv_func, args=(sock,))
+t.start()
+
+# 如果收到了对方的ip和端口，就开始下一步动作
+while True:
+    data = {"COMMAND": "get_peer", "key": "zhb1"}
+    msg = json.dumps(data).encode()
+    sent = sock.sendto(msg, ('175.178.109.217', 8478))
+    time.sleep(1)
+
+    peers=json.loads(recv_msg)
+
+    if len(peers)==2:
+        break
+
+print(peers)
+
+for peer in peers:
+    if peer["identifier"]==identifier:
+        self_peer=peer
+    if peer["identifier"]!=identifier:
+        other_peer=peer
+
+self_ip=self_peer["ip"]
+self_port=self_peer["port"]
+
+other_ip=other_peer["ip"]
+other_port=other_peer["port"]
+
+print(other_ip)
+print(other_port)
+
+for i in range(10):
+    sent = sock.sendto(b"0", (other_ip, other_port))
+    time.sleep(1)
+
+# while True:
+#     msg=f"{identifier}:hello!"
+#     sent = sock.sendto(msg.encode(), (other_ip, other_port))
+#     time.sleep(1)
+#     print("send success!")
+
+print("end")
